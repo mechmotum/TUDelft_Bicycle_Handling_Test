6fef07c1cb2699598dfb7ee6fa4a0dd447bebc20
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2022-08-30 14:31:36 +0200

    Update startHandlingTest.ino

--- Arduino Uno/startHandlingTest/startHandlingTest.ino
+++ Arduino Uno/startHandlingTest/startHandlingTest.ino
@@ -2,17 +2,17 @@ int LEDpin = 2;
 int BUTTONpin = 3;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LEDpin, OUTPUT);
   pinMode(BUTTONpin, INPUT_PULLUP);
-  digitalWrite(LEDpin, HIGH);
+  digitalWrite(LEDpin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int state = digitalRead(BUTTONpin);
-  Serial.println(!state);
-  digitalWrite(LEDpin, !state);
+  int state = digitalRead(BUTTONpin); // Read the button's state
+  // Button state is 1 when not pressed, 0 when pressed
+  // So we need to invert it in Serial and LEDs
+  Serial.println(!state); // Print the button's state to serial
+  digitalWrite(LEDpin, !state); // Turn on the button
   delay(50);
 }
