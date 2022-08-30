46fb705bfb8cf6891825ba0891c93c2148649b48
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2022-08-30 15:19:38 +0200

    Update GatesLoc.cs

--- Unity Project/Assets/Scripts/GatesLoc.cs
+++ Unity Project/Assets/Scripts/GatesLoc.cs
@@ -4,26 +4,25 @@ using System.IO;
 
 public class GatesLoc : MonoBehaviour
 {	
-	static int numOfPoints = 375;
-	static int numOfLines = 4501; //3201
+	static int numOfPoints = 375; // Number of points along the horizon (75 Hz sample rate * 5 sec)
+	static int numOfLines = 4501; // Number of lines in the reference file
 	string[] allGates = new string[numOfLines];
-	public int gateZloc;
+	public int gateZloc; // Z is the axis towards the horizon
 	public bool gateFound = false;
-	public float gateXloc;
-	
-	//int DebugCount = 0;
+	public float gateXloc; // X is the axis from side to side
    			       
 	void Start() 
 	{
+		// Store the file in StreamingAssets
 		string fileLocation = Path.Combine(Application.streamingAssetsPath, "gate_locations.txt");
 		StreamReader reader = new StreamReader(fileLocation);
-		// Read the file and save all gate locations
+		// Read the file and save all lines into one array
 	    	for (int i = 0; i < numOfLines; i++) 
 	    	{
 		       	string gateString = reader.ReadLine();
 			allGates[i] = gateString;
 	    	}
-		// Extract the gates at t=0
+		// Extract the reference at t=0
 		string[] allGateX = allGates[0].Trim().Split(',');
 		Vector3 gateLoc = new Vector3();
 		// Search if there are gates at this point in time
@@ -36,13 +35,15 @@ public class GatesLoc : MonoBehaviour
 			{
 				// Gate found, save its X position and Z index
 				gateXloc = float.Parse(allGateX[i]);
-				x = gateXloc / 1.5f;
+				x = gateXloc / 1.5f; // Divide by 1.5 since the Scale of the ground is set to 1.5
 				gateZ = i;
 				gateZloc = i;
 				gateFound = true;
-				break; // Stop looking for other gates
+				break; // Stop looking for other gates 
+				// TODO: make this work with multiple gates
 			}
 		}
+		// Display the gate
 		if (gateFound) 
 		{
 			float z = (1f / (float)(numOfPoints-1)) * gateZ - 0.5f; // Calculate Z position
@@ -58,9 +59,8 @@ public class GatesLoc : MonoBehaviour
     	void Update()
     	{
 		int currentLine = GameObject.Find("UDPComms").GetComponent<UDPReceive>().currLine;
-    		// Extract and render the reference at current time
+    		// Extract the reference at current time
 		string[] allGateX = allGates[currentLine].Trim().Split(',');
-		//string[] allGateX = allGates[DebugCount].Trim().Split(',');
     		Vector3 gateLoc = new Vector3();
     		// Search if there are gates at this point in time
 		gateFound = false;
@@ -72,13 +72,15 @@ public class GatesLoc : MonoBehaviour
 			{
 				// Gate found, save its X position and Z index
 				gateXloc = float.Parse(allGateX[i]);
-				x = gateXloc / 1.5f;
+				x = gateXloc / 1.5f; // Divide by 1.5 since the Scale of the ground is set to 1.5
 				gateZ = i;
 				gateZloc = i;
 				gateFound = true;
 				break; // Stop looking for other gates
+				// TODO: make this work with multiple gates
 			}
 		}
+		// Display the gare
 		if (gateFound) 
 		{
 			float z = (1f / (float)(numOfPoints-1)) * gateZ - 0.5f; // Calculate Z position
@@ -89,6 +91,5 @@ public class GatesLoc : MonoBehaviour
 			gateLoc.Set(0.0f, 1.35f, -0.6f); // If no gate, place it behind the bicycle
 		}
     		transform.localPosition = gateLoc; // Render
-		//DebugCount = (DebugCount + 1) % numOfLines;
     	}
 }
